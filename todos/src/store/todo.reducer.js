b2c632ba9e002af1883de2469fb76be6dc43e80d
ctyever <ctyever@naver.com>
2021-06-13 21:07:52 +0900

    20210613

--- todos/src/store/todo.reducer.js
+++ todos/src/store/todo.reducer.js
@@ -8,7 +8,7 @@ const todoReducer = (state = initialState, action) => {
         case "TOGGLE_TODO": return {...state, todos: state.todos.map(todo => (todo.id === action.payload) 
                                                                                 ? {...todo, complete: !todo.complete} 
                                                                                 : todo)} 
-        case "DELETE_TODO": return {...state, todos: state.todos.filter(todo => todo.id == action.payload)}
+        case "DELETE_TODO": return {...state, todos: state.todos.filter(todo => todo.id !== action.payload)}
         default: return state
     }
 }
